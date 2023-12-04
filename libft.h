1a4ba0f822e881972bdecd419ecbf892cbc86c73
Kerem Narman <knarman@k2m11s08.42kocaeli.com.tr>
2023-12-04 17:06:09 +0300

    first commit

---
+++ libft.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knarman <knarman@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 15:56:33 by knarman           #+#    #+#             */
+/*   Updated: 2023/12/04 16:48:09 by knarman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <unistd.h>
+
+int ft_isalpha(int c);
+
+int ft_isalpha(int c);
+
+int ft_isascii(int c);
+
+int ft_isdigit(int c);
+
+int ft_isprint(int c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
